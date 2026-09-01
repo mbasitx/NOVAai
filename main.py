21e27fc227655cb418b5a8222f721e494f395b17
mbasitx <mydevices027@gmail.com>
2026-09-01 10:48:32 +0500

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -466,4 +466,4 @@ async def webhook(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
+    uvicorn.main(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
